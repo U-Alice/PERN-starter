13c4de45bc405e4c6a771a208f8e98e6f3f981b9
U_Alice <umugwanezaalice22@gmail.com>
2024-06-16 23:53:06 +0200

    [feat]: searching

--- frontend-service/src/components/PaginatedTable.jsx
+++ frontend-service/src/components/PaginatedTable.jsx
@@ -1,17 +1,30 @@
 import React, { useEffect, useState } from "react";
 import Table from "./table";
-import { Pagination } from "./pagination";
 
 const PaginatedTable = ({ data, itemsPerPage, headers }) => {
   const [currentPage, setCurrentPage] = useState(1);
 
   const [totalPages, setTotalPages] = useState(0);
 
+  const [searchQuery, setSearchQuery] = useState("");
+
   const handleNext = () => {
     if (totalPages > currentPage) setCurrentPage((current) => current + 1);
   };
   const handlePrevious = () => {
     if (currentPage > 1) setCurrentPage((current) => current - 1);
+  };
+
+   const filteredData = data.filter((item) =>
+     Object.values(item).some((value) =>
+       String(value).toLowerCase().includes(searchQuery.toLowerCase())
+     )
+   );
+
+
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value);
+    setCurrentPage(1); 
   };
 
   useEffect(() => {
@@ -21,7 +34,8 @@ const PaginatedTable = ({ data, itemsPerPage, headers }) => {
   return (
     <div className="w-full h-full">
       <Table
-        data={data}
+        handleSearch={handleSearch}
+        data={filteredData}
         currentPage={currentPage}
         itemsPerPage={itemsPerPage}
         headers={headers}
